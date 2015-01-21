608e9fd1802bfc7c54f1b1c943c44ab764a6bc08
Dorimanx <yuri@bynet.co.il>
2015-01-21 18:30:37 +0200

    F320K 21P KK Clean LG Source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Thu May 8 11:25:14 KST 2014"
+#define LINUX_COMPILE_BY "kyeongsik7.kim"
+#define LINUX_COMPILE_HOST "LGEARND12B1"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
